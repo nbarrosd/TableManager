47b111ad5c0293644b6d77a26e9a3567e40e270b
xg2401 <54091316+xg2401@users.noreply.github.com>
2022-09-07 21:56:20 -0300

    Create index.blade.php
    prototype of the index main page, will change it as soon as we have all our work together

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prototipo</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <table>
+        <thead>
+            <th>Fecha ingreso</th>
+            <th>Visitante</th>
+            <th>Nro Documento</th>
+            <th>Oficina</th>
+            <th>Empleado</th>
+            <th>Motivo</th>
+            <th>Fecha de salida</th>
+            <th>Accion</th>
+        </thead>
+    <tbody>
+        @foreach($listado as $l)
+    <tr>
+        <td>{{$l->fecha}}</td>
+        <td>{{$l->$nombres}}</td>
+        <td><a class="btn btn-warning">Editar</a> <a class="btn btn-success">detalles</a></td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
+</body>
+</html>
